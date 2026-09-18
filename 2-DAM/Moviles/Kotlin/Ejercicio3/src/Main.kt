991943eb82177f057ceecd7d07472b652b9deaa3
Mario Sanchez <mariosp860@gmail.com>
2026-09-18 12:13:14 +0200

    Refactorizo el ejercicio 3 de moviles

--- 2-DAM/Moviles/Kotlin/Ejercicio3/src/Main.kt
+++ 2-DAM/Moviles/Kotlin/Ejercicio3/src/Main.kt
@@ -1,43 +1,24 @@
 fun main() {
-    val numerosLoteria = mutableListOf<Int>()
-    var i = 0;
+    val numerosLoteria = mutableSetOf<Int>()
     println("------------ SORTEO LOTERIA -----------------")
     println("Porfavor introduzca 6 numero entre 0 y 50 ambos incluidos")
     println("----------------------------------------------------------")
     while (numerosLoteria.size < 6) {
         var numero = 0;
         try {
-            do {
-                print("Dime el numero ${i + 1}: ")
-                numero = readln().toInt()
-                if (numero !in 0..50) {
-                    println("El numero $numero debe de estar entre 0 y 50.")
-                    continue
-                }
-                numerosLoteria.forEach {
-                    if (numero == it) {
-                        println("El numero $numero esta repetido.")
-                        break;
-                    }
-                }
-                numerosLoteria.add(numero);
-                i++;
-            } while ((numero !in 0..50))
+            print("Dime el numero ${numerosLoteria.size + 1}: ")
+            numero = readln().toInt()
+            if (numero !in 0..50) {
+                println("El numero $numero debe de estar entre 0 y 50.")
+                continue
+            }
+            numerosLoteria.add(numero);
         } catch (e: NumberFormatException) {
             println(e.toString())
         }
     }
     println()
-    var j = 0;
     println("Boleto completado. Tus numeros de loteria son: ")
-    numerosLoteria.forEach {
-        j++;
-        if (j < 6) {
-            print("$it * ")
-        } else {
-            println("$it")
-        }
+    println(numerosLoteria.joinToString(separator = " * "))
 
-
-    }
 }
